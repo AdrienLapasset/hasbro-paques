a50804c8820dff71e3ec3999ccbdd4c7e57f27d5
Klemo <clement.lapasset@gmail.com>
2022-02-01 10:26:23 +0100

    kikou

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ includeHeader('ishome')
     <h1>Pages :</h1>
     <div class="list-group">
       <a class="list-group-item list-group-item-action" href="home.php">Home</a>
-      <p>kikou</p>
     </div>
   </div>
 </div>
