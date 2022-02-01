23d02dbe090569fc8cd4092c3cd8ed1f9b036ab5
Adrien Lapasset <aflapasset@gmail.com>
2022-02-01 14:09:03 +0100

    add toggle menu

--- partials/footer.php
+++ partials/footer.php
@@ -34,9 +34,19 @@
 <!-- Bootstrap JS import -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-<!-- Slick Carousel JS import -->
+<!-- JQuery JS import -->
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.open-menu-btn').click(function() {
+      $('.menu-nav').css("transform", "translateX(0px)")
+    })
+    $('.close-menu-btn').click(function() {
+      $('.menu-nav').css("transform", "translateX(200px)")
+    })
+  });
+</script>
 
 </body>
 
