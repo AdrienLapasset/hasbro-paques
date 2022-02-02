b204329d02d12c913b509e91f276ade1ad7ef368
Adrien Lapasset <aflapasset@gmail.com>
2022-02-02 16:28:24 +0100

    check box styles

--- creer-compte.php
+++ creer-compte.php
@@ -29,17 +29,20 @@ includeHeader('')
         <img src="assets/icons/pen.svg" alt="">
       </div>
     </div>
-    <p class="mentions-obligatoires">* Mentions obligatoires</p>
+    <p class="mentions">* Mentions obligatoires</p>
     <label class="checkbox-container">
-      One
+      J’ai lu et j’accepte le règlement du jeu*
       <input type="checkbox">
       <span class="checkmark"></span>
     </label>
-    <label class="checkbox-container">two
+    <label class="checkbox-container">
+      J’accepte que mes données personnelles soient utilisées dans
+      le cadre du jeu*
       <input type="checkbox">
       <span class="checkmark"></span>
     </label>
-    <button type="submit" class="btn btn-yellow mx-auto mt-4">Valider les modifications</button>
+    <p class="mentions">Take Off, société de gestion, prestataire de services, met en œuvre des traitements de données à caractère personnel ayant pour finalités la conservation des preuves d’achats, la gestion et le suivi de l’organisation de l’opération du jeu, la vérification et le contrôle du respect des dispositions du règlement du jeu, l’analyse de la régularité de la participation, la gestion de la relation avec les participants et gagnants, la désignation des gagnants et la remise des lots, le pilotage, le reporting et les statistiques. Vous disposez d’un droit d’accès, de rectification, de portabilité, d’effacement de vos données à caractère personnel et de limitation du traitement de vos données ainsi que d’un droit d’opposition et du droit de définir des directives relatives à la conservation, à l’effacement et à la communication de vos données à caractère personnel. Pour en savoir plus sur la protection de vos données, consultez l’article 12 du règlement.</p>
+    <button type="submit" class="btn btn-yellow mx-auto mt-4">Valider</button>
   </form>
 </main>
 
