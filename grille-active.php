7a8f600bcc4ee80e27ac0ca60ce0f291f095c342
Klemo <clement.lapasset@gmail.com>
2022-02-02 10:55:45 +0100

    Init Grille

--- grille-active.php
+++ grille-active.php
@@ -7,88 +7,170 @@ includeHeader('')
 ?>
 
 <main class="grille-active">
- <div class="grid-container">
-    <input id="box1" type="checkbox">
-    <label for="box1">1</label>
-    <input id="box2" type="checkbox">
-    <label for="box2">2</label>
-    <input id="box3" type="checkbox">
-    <label for="box3">3</label>
-    <input id="box4" type="checkbox">
-    <label for="box4">4</label>
-    <input id="box5" type="checkbox">
-    <label for="box5">5</label>
-    <input id="box6" type="checkbox">
-    <label for="box6">6</label>
-    <input id="box7" type="checkbox">
-    <label for="box7">7</label>
-    <input id="box8" type="checkbox">
-    <label for="box8">8</label>
-    <input id="box9" type="checkbox">
-    <label for="box9">9</label>
-    <input id="box10" type="checkbox">
-    <label for="box10">10</label>
-    <input id="box11" type="checkbox">
-    <label for="box11">11</label>
-    <input id="box12" type="checkbox">
-    <label for="box12">12</label>
-    <input id="box13" type="checkbox">
-    <label for="box13">13</label>
-    <input id="box14" type="checkbox">
-    <label for="box14">14</label>
-    <input id="box15" type="checkbox">
-    <label for="box15">15</label>
-    <input id="box16" type="checkbox">
-    <label for="box16">16</label>
-    <input id="box17" type="checkbox">
-    <label for="box17">17</label>
-    <input id="box18" type="checkbox">
-    <label for="box18">18</label>
-    <input id="box19" type="checkbox">
-    <label for="box19">19</label>
-    <input id="box20" type="checkbox">
-    <label for="box20">20</label>
-    <input id="box21" type="checkbox">
-    <label for="box21">21</label>
-    <input id="box22" type="checkbox">
-    <label for="box22">22</label>
-    <input id="box23" type="checkbox">
-    <label for="box23">23</label>
-    <input id="box24" type="checkbox">
-    <label for="box24">24</label>
-    <input id="box25" type="checkbox">
-    <label for="box25">25</label>
-    <input id="box26" type="checkbox">
-    <label for="box26">26</label>
-    <input id="box27" type="checkbox">
-    <label for="box27">27</label>
-    <input id="box28" type="checkbox">
-    <label for="box28">28</label>
-    <input id="box29" type="checkbox">
-    <label for="box29">29</label>
-    <input id="box30" type="checkbox">
-    <label for="box30">30</label>
-    <input id="box31" type="checkbox">
-    <label for="box31">31</label>
-    <input id="box32" type="checkbox">
-    <label for="box32">32</label>
-    <input id="box33" type="checkbox">
-    <label for="box33">33</label>
-    <input id="box34" type="checkbox">
-    <label for="box34">34</label>
-    <input id="box35" type="checkbox">
-    <label for="box35">35</label>
-    <input id="box36" type="checkbox">
-    <label for="box36">36</label>
-    <input id="box37" type="checkbox">
-    <label for="box37">37</label>
-    <input id="box38" type="checkbox">
-    <label for="box38">38</label>
-    <input id="box39" type="checkbox">
-    <label for="box39">39</label>
-    <input id="box40" type="checkbox">
-    <label for="box40">40</label>
- </div>
+  <div class="d-flex justify-content-center">
+    <div class="grid-container">
+      <div class="box">
+        <input id="box1" type="checkbox">
+        <label for="box1">1</label>
+      </div>
+       <div class="box">
+         <input id="box2" type="checkbox">
+         <label for="box2">2</label>
+       </div>
+       <div class="box">
+         <input id="box3" type="checkbox">
+         <label for="box3">3</label>
+       </div>
+       <div class="box">
+         <input id="box4" type="checkbox">
+         <label for="box4">4</label>
+       </div>
+       <div class="box">
+         <input id="box5" type="checkbox">
+         <label for="box5">5</label>
+       </div>
+       <div class="box">
+         <input id="box6" type="checkbox">
+         <label for="box6">6</label>
+       </div>
+       <div class="box">
+         <input id="box7" type="checkbox">
+         <label for="box7">7</label>
+       </div>
+   <div class="box">
+     <input id="box8" type="checkbox">
+     <label for="box8">8</label>
+   </div>
+       <div class="box">
+         <input id="box9" type="checkbox">
+         <label for="box9">9</label>
+       </div>
+   <div class="box">
+     <input id="box10" type="checkbox">
+     <label for="box10">10</label>
+       </div>
+   <div class="box">
+     <input id="box11" type="checkbox">
+     <label for="box11">11</label>
+       </div>
+       <div class="box">
+         <input id="box12" type="checkbox">
+         <label for="box12">12</label>
+       </div>
+       <div class="box">
+         <input id="box13" type="checkbox">
+         <label for="box13">13</label>
+       </div>
+       <div class="box">
+         <input id="box14" type="checkbox">
+         <label for="box14">14</label>
+       </div>
+       <div class="box">
+         <input id="box15" type="checkbox">
+         <label for="box15">15</label>
+       </div>
+       <div class="box">
+         <input id="box16" type="checkbox">
+         <label for="box16">16</label>
+       </div>
+       <div class="box">
+         <input id="box17" type="checkbox">
+         <label for="box17">17</label>
+       </div>
+       <div class="box">
+         <input id="box18" type="checkbox">
+         <label for="box18">18</label>
+       </div>
+       <div class="box">
+         <input id="box19" type="checkbox">
+         <label for="box19">19</label>
+       </div>
+       <div class="box">
+         <input id="box20" type="checkbox">
+         <label for="box20">20</label>
+       </div>
+       <div class="box">
+         <input id="box21" type="checkbox">
+         <label for="box21">21</label>
+       </div>
+       <div class="box">
+         <input id="box22" type="checkbox">
+         <label for="box22">22</label>
+       </div>
+       <div class="box">
+         <input id="box23" type="checkbox">
+         <label for="box23">23</label>
+       </div>
+       <div class="box">
+         <input id="box24" type="checkbox">
+         <label for="box24">24</label>
+       </div>
+       <div class="box">
+         <input id="box25" type="checkbox">
+         <label for="box25">25</label>
+       </div>
+       <div class="box">
+         <input id="box26" type="checkbox">
+         <label for="box26">26</label>
+       </div>
+       <div class="box">
+         <input id="box27" type="checkbox">
+         <label for="box27">27</label>
+       </div>
+       <div class="box">
+         <input id="box28" type="checkbox">
+         <label for="box28">28</label>
+       </div>
+       <div class="box">
+         <input id="box29" type="checkbox">
+         <label for="box29">29</label>
+       </div>
+       <div class="box">
+         <input id="box30" type="checkbox">
+         <label for="box30">30</label>
+       </div>
+       <div class="box">
+         <input id="box31" type="checkbox">
+         <label for="box31">31</label>
+       </div>
+       <div class="box">
+         <input id="box32" type="checkbox">
+         <label for="box32">32</label>
+       </div>
+       <div class="box">
+         <input id="box33" type="checkbox">
+         <label for="box33">33</label>
+       </div>
+       <div class="box">
+         <input id="box34" type="checkbox">
+         <label for="box34">34</label>
+       </div>
+       <div class="box">
+         <input id="box35" type="checkbox">
+         <label for="box35">35</label>
+       </div>
+       <div class="box">      
+         <input id="box36" type="checkbox">
+         <label for="box36">36</label>
+       </div>
+       <div class="box">
+         <input id="box37" type="checkbox">
+         <label for="box37">37</label>
+       </div>
+       <div class="box">
+         <input id="box38" type="checkbox">
+         <label for="box38">38</label>
+       </div>
+       <div class="box">
+         <input id="box39" type="checkbox">
+         <label for="box39">39</label>
+       </div>
+       <div class="box">
+         <input id="box40" type="checkbox">
+         <label for="box40">40</label>
+       </div>
+    </div>
+  </div>
 </main>
 
 <?php include 'partials/footer.php'; ?>
