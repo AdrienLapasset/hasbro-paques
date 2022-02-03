adb3ce9b01ab7b9cfd47d7ad7595c01a51ddc470
Adrien Lapasset <aflapasset@gmail.com>
2022-02-03 13:18:15 +0100

    home-1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <h1>Pages :</h1>
     <div class="list-group">
       <a class="list-group-item list-group-item-action" href="teasing.php">Teasing</a>
-      <a class="list-group-item list-group-item-action" href="home.php">Home - 1</a>
+      <a class="list-group-item list-group-item-action" href="home-1.php">Home - 1</a>
       <a class="list-group-item list-group-item-action" href="home-2.php">Home - 2</a>
       <a class="list-group-item list-group-item-action" href="mon-compte.php">Mon compte</a>
       <a class="list-group-item list-group-item-action" href="creer-compte.php">Creer compte</a>
