273d9bc2c16d5093ca25b8244fe66965cdb6eb42
Adrien Lapasset <aflapasset@gmail.com>
2022-02-03 16:24:41 +0100

    fix

--- home-1.php
+++ home-1.php
@@ -3,9 +3,9 @@
 <main class="home">
   <img class="img-fluid d-none d-sm-block" src="assets/imgs/KV_POOL.jpg" alt="">
   <img class="img-fluid d-sm-none" src="assets/imgs/KV2_MOBILE.jpg" alt="">
-  <div class="d-flex flex-column flex-lg-row mt-5">
-    <a href="" class="btn btn-primary me-lg-2 mb-3 mb-lg-0">Je crée un compte</a>
-    <a href="" class="btn btn-primary ms-lg-2">Je me connecte</a>
+  <div class="d-flex flex-column flex-md-row mt-5">
+    <a href="" class="btn btn-primary me-md-2 mb-3 mb-md-0">Je crée un compte</a>
+    <a href="" class="btn btn-primary ms-md-2">Je me connecte</a>
   </div>
 </main>
 
