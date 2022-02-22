388770f4cf86f292043a5e1ac41f9e52f775b725
Klemo <clement.lapasset@gmail.com>
2022-02-22 14:00:08 +0100

    Minor title changes Rules & Legal notice

--- mentions-legales.php
+++ mentions-legales.php
@@ -9,14 +9,14 @@ includeHeader('isNav', 'mentions')
 <main class="mentions-legales-reglement">
   <h1>Mentions légales</h1>
   <div class="container">
-    <h1 class="p-title">1. Informations légales</h1>
+    <h2 class="p-title">1. Informations légales</h2>
     <b>Éditeur du site</b>
-    <p>Le site <a href="www.loto-paques-hasbro.fr">www.loto-paques-hasbro.fr</a>, ci-après le Site, est édité pour la Société HASBRO FRANCE, S.A.S au capital de 1.700.000 Euros, immatriculée au Registre du Commerce et des Sociétés de Chambéry sous le n° 746 220 623 dont le siège social est situé au 6 Avenue du Lac d’Annecy, Savoie-Technolac, 73370 Le Bourget-du-Lac.</p>
+    <p>Le site <a href="https://www.loto-paques-hasbro.fr">www.loto-paques-hasbro.fr</a>, ci-après le Site, est édité pour la Société HASBRO FRANCE, S.A.S au capital de 1.700.000 Euros, immatriculée au Registre du Commerce et des Sociétés de Chambéry sous le n° 746 220 623 dont le siège social est situé au 6 Avenue du Lac d’Annecy, Savoie-Technolac, 73370 Le Bourget-du-Lac.</p>
     <b>Conception du site</b>
     <p>Le Site est créé par la société Les Envahisseurs, SARL au capital de 200 000€ dont le siège social est situé 13B Place Jules Ferry – 69006 - LYON – France.</p>
     <b>Hébergeur du site</b>
     <p>Le Site est hébergé par la société Jaguar Network, situé au 71 avenue André Roussin, 13321 Marseille CEDEX 16.</p>
-    <h1 class="p-title">2. Données personnelles</h1>
+    <h2 class="p-title">2. Données personnelles</h2>
     <p>La Société Organisatrice s'engage à respecter votre vie privée et à protéger les informations collectées sur le Site.</p>
     <p>Les informations personnelles recueillies font l’objet d’un traitement informatique par la société Take Off, prestataire de services de la société Les Envahisseurs. Ce traitement est réalisé uniquement en France.</p>
     <p>Les données collectées sont indispensables à ce traitement et sont conservées pour une durée d’un an à compter de la clôture de cette opération.</p>
@@ -33,20 +33,20 @@ includeHeader('isNav', 'mentions')
         </p>
     </p>
     <p>Le traitement de données a fait l’objet d’une déclaration auprès de la CNIL sous le N°2014254V0.</p>
-    <h1 class="p-title">3. Propriété Intellectuelle</h1>
+    <h2 class="p-title">3. Propriété Intellectuelle</h2>
     <p>L'accès au Site vous confère un droit d'usage privé et non exclusif de ce site.</p>
     <p>Tous les articles, photographies et autres documents sur ce site sont la propriété de la société HASBRO ou sont autorisés avec l'autorisation de leurs propriétaires, et sont soumis au droit d'auteur et autres droits de propriété intellectuelle.</p>
     <p>Les marques de commerce, logos, personnages (désignés ensemble "Marque") présents sur ce site Internet appartiennent à la Société Organisatrice. Aucune interprétation ne peut être faite en faveur d'une licence d'utilisation ou d'un quelconque droit d'utiliser la Marque affichée sur le site Internet.</p>
     <p>L'utilisation frauduleuse de la Marque ou de tout autre contenu du site Internet est totalement interdite.</p>
-    <h1 class="p-title">4. Accès au Site</h1>
+    <h2 class="p-title">4. Accès au Site</h2>
     <p>La Société Organisatrice s'efforce de maintenir accessible le site, sans pour autant être tenue à aucune obligation d'y parvenir. Il est précisé qu'à des fins de maintenance, de mise à jour et pour toute autre raison, notamment d'ordre technique, l'accès au site pourra être interrompu. La Société Organisatrice n'est en aucun cas responsable de ces interruptions et des conséquences qui peuvent en découler pour l'utilisateur.</p>
-    <h1 class="section-title">POLITIQUE DE GESTION DES COOKIES</h1>
-    <h1 class="p-title">Définition du Cookie</h1>
+    <h2 class="section-title">POLITIQUE DE GESTION DES COOKIES</h2>
+    <h2 class="p-title">Définition du Cookie</h2>
     <p>Un cookie est un petit fichier texte qui est placé ou stocké sur votre terminal (ordinateur, tablette ou appareil mobile) lorsque vous visitez un site internet. Le cookie permet à un site de vous reconnaître, de vous aider à naviguer de page en page sur un site internet, fournit des connexions sécurisées et se souvient de vos préférences lors de vos prochaines visites.</p>
     <p>La Société Conceptrice du site a recours à l'utilisation de cookies afin d’améliorer continuellement l’expérience des visiteurs de son Site Web. Les cookies peuvent directement améliorer cette expérience ou peuvent même être nécessaires pour effectuer correctement et légalement les services que vous demandez (p. ex. retenir votre sélection de paramètres en matière de cookies). De plus, certains cookies fournissent à la Société Organisatrice des données statistiques sur l'utilisation qui est faite de son Site Web, ce qui lui permet, de façon indirecte, de mieux répondre aux préférences des visiteurs en s'y adaptant.</p>
-    <h1 class="p-title">Suppression des cookies</h1>
+    <h2 class="p-title">Suppression des cookies</h2>
     <p>L’Utilisateur peut à tout moment désactiver les cookies enregistrés sur son terminal. Pour cela, il lui suffit de sélectionner les paramètres appropriés dans son navigateur. Cependant, cette désactivation aura pour conséquence d'empêcher l'accès à certaines fonctionnalités du Site permettant de personnaliser les services proposés.</p>
-    <h1 class="p-title">Les différents types de cookies utilisés</h1>
+    <h2 class="p-title">Les différents types de cookies utilisés</h2>
     <ul>
       <li>
         <b>Les cookies strictement nécessaires </b>: ces cookies vous permettent de naviguer sur le Site et sont indispensables au bon fonctionnement du Site. Leur désactivation entraînera des difficultés dans l’utilisation du Site et aura pour conséquence d'empêcher l'accès à certaines fonctionnalités.
@@ -58,9 +58,9 @@ includeHeader('isNav', 'mentions')
         <b>Les cookies de performance et statistiques </b>: ces cookies collectent des informations sur la façon dont les Utilisateurs utilisent le Site (le nombre de visites, le nombre de pages consultées, l’activité des visiteurs) via les outils d’analyse Google Analytics. Ces cookies nous permettent également d’identifier et de résoudre les problèmes de fonctionnement du Site et de l’améliorer.
       </li>
     </ul>
-    <h1 class="section-title">La gestion des cookies</h1>
+    <h2 class="section-title">La gestion des cookies</h2>
     <p>Pour la gestion des cookies et de vos choix, la configuration de chaque navigateur est différente. Elle est décrite dans le menu d'aide de votre navigateur, qui vous permettra de savoir de quelle manière modifier vos souhaits en matière de cookies.</p>
-    <h1 class="p-title">Désactivation des cookies via les principaux navigateurs :</h1>
+    <h2 class="p-title">Désactivation des cookies via les principaux navigateurs :</h2>
     <i>Google Chrome</i>
     <ol>
       <li>Dans le menu, cliquez sur « Paramètres » puis sur « Afficher les paramètres avancés »</li>
@@ -87,7 +87,7 @@ includeHeader('isNav', 'mentions')
       <li>Cliquez sur le bouton « Avancé » pour faire apparaitre la fenêtre « Paramètres de confidentialité avancés ».</li>
       <li>Cochez ensuite la case « Ignorer la gestion automatique des cookies », puis sélectionnez « Refuser » et sauvegardez vos préférences en cliquant sur « OK ».</li>
     </ol>
-    <h1 class="p-title">Module de gestion de cookies</h1>
+    <h2 class="p-title">Module de gestion de cookies</h2>
     <p>La société Conceptrice du Site met à la disposition des Utilisateurs du Site un module leur permettant de s’opposer aux cookies déposés sur le Site. Pour ce faire, l’Utilisateur peut cliquer sur le bouton « Je personnalise » présent dans le bandeau d’information mentionnant les finalités des cookies déposés.</p>
   </div>
 </main>
--- reglement.php
+++ reglement.php
@@ -12,9 +12,9 @@ includeHeader('isNav', 'mentions')
     <span style="text-transform: none;">« Le Grand loto de Pâques »</span>
   </h1>
   <div class="container">
-    <h1 class="p-title">ARTICLE 1 – Société Organisatrice</h1>
+    <h2 class="p-title">ARTICLE 1 – Société Organisatrice</h2>
     <p>L’agence Les Envahisseurs 433 999 257 RCS LYON, SASU Société par actions simplifiée à associé unique (ci-après désignée « la Société gestionnaire »), organise pour le compte de la société HASBRO FRANCE, S.A.S au capital de 1.700.000 Euros, immatriculée au Registre du Commerce et des Sociétés de Chambéry sous le n° 746 220 623 dont le siège social est situé à l’adresse suivante Savoie-Technolac, 73370 Le Bourget-du-Lac (ci-après désignée la "Société Organisatrice") un jeu avec obligation d’achat (ci-après dénommé le « jeu »), valable du 26/03/2022 au 17/04/2022 inclus selon les modalités décrites dans le présent règlement (ci-après dénommé « Règlement »).</p>
-    <h1 class="p-title">ARTICLE 2 – Conditions d’accès au jeu</h1>
+    <h2 class="p-title">ARTICLE 2 – Conditions d’accès au jeu</h2>
     <p>Le jeu est ouvert à toute personne physique majeure résidant en France Métropolitaine, Corse incluse disposant d’un accès internet et d’une adresse électronique valide.  A l’exception des membres du personnel de la Société Organisatrice, des prestataires ayant participé à la mise en place du jeu et de leur famille.</p>
     <div>Le jeu est porté à la connaissance du public sur les supports suivants :
       <ul>
@@ -24,11 +24,11 @@ includeHeader('isNav', 'mentions')
         <li>Site affinitaire</li>
       </ul>
     </div>
-    <h1 class="p-title">ARTICLE 3 – Limite de participation</h1>
+    <h2 class="p-title">ARTICLE 3 – Limite de participation</h2>
     <p>Le jeu est limité à un compte par foyer (même nom, même adresse, même e-mail, même RIB) sur toute la durée du jeu. Le nombre maximum de grilles qu’un participant pourra se voir attribuer est <b>limité à 7</b> et l’achat de produits supplémentaires ne donne pas droit à l’attribution de grilles supplémentaires ou plus de chances de gagner.</p>
     <p>Le Jeu se déroule exclusivement via le site accessible à l’adresse : www.loto-paques-hasbro.fr à l’exclusion de tout autre moyen de participation.</p>
     <p>Pour bénéficier de son lot, le participant doit être titulaire d’un compte bancaire ouvert en France et doit posséder une pièce d’identité valable.</p>
-    <h1 class="p-title">ARTICLE 4 – Modalités de participation</h1>
+    <h2 class="p-title">ARTICLE 4 – Modalités de participation</h2>
     <p style="text-decoration:underline;">Pour jouer les participants devront :</p>
     <p><b>1/ Acheter</b> entre le 26/03/2022 et le 17/04/2022 inclus (date du ticket de caisse ou facture faisant foi) en une seule fois ou plusieurs fois, un à trois jouets Hasbro ou un œuf géant Hasbro parmi les marques : Marvel, Star Wars, Transformers, Power Rangers, Beyblade Burst, Hasbro Gaming, Monopoly, Play Doh, Disney Princess, Disney Frozen, My Little Poney, Pjmasks, Nerf, Peppa Pig, Furreal Friends et Potato Head.</p>
     <p>En fonction du nombre de produits achetés, le participant se voit attribuer une ou plusieurs grilles de loto, palier précisé ci-dessous :</p>
@@ -39,7 +39,7 @@ includeHeader('isNav', 'mentions')
       <li>1 œuf géant = 7 grilles de loto</li>
     </ul>
     <p>Sur chacune des grilles, il est possible de sélectionner une combinaison de 6 numéros entre 1 et 40. <b><i>Dans la limite de 7 grilles maximum par personne.</i></b></p>
-    <p><b>2/ Se connecter</b> sur le site <a href="www.loto-paques-hasbro.fr">www.loto-paques-hasbro.fr</a> ou <b>flasher</b> le QR code présent sur certains supports entre le 26/03/2022 00h01 et le 17/04/2022 23h59 inclus (date et heure en France Métropolitaine) muni d’un scan ou de la photo de son ticket de caisse ou facture et du / des codes-barres du/des produit(s) acheté(s).</p>
+    <p><b>2/ Se connecter</b> sur le site <a href="https://www.loto-paques-hasbro.fr">www.loto-paques-hasbro.fr</a> ou <b>flasher</b> le QR code présent sur certains supports entre le 26/03/2022 00h01 et le 17/04/2022 23h59 inclus (date et heure en France Métropolitaine) muni d’un scan ou de la photo de son ticket de caisse ou facture et du / des codes-barres du/des produit(s) acheté(s).</p>
     <p><b>3/ Procéder à son inscription en ligne</b> en remplissant les champs obligatoires du formulaire d’inscription (nom, prénom, adresse postale complète, email, numéro de téléphone, mot de passe) au jeu puis valider pour s’enregistrer et créer son compte.</p>
     <ul>
       <li>Lire et accepter le présent règlement en cochant la case prévue à cet effet.</li>
@@ -55,7 +55,7 @@ includeHeader('isNav', 'mentions')
     <p>En fonction du nombre de produits achetés, une ou plusieurs grilles seront automatiquement disponibles dans l’onglet « Mes grilles ».</p>
     <p><b>5/ Choisir sa combinaison de 6 numéros</b> sur une grille puis valider son choix. Attention toute validation est définitive, il sera impossible de modifier ses choix de numéros.</p>
     <p>Ne seront pas prises en considération les participations dont les coordonnées ou informations seront incomplètes, fausses, incompréhensibles, adressées après la date limite de participation ou contraires aux dispositions du présent règlement. Ne seront pas pris en considération les participants jouant un nombre de fois supérieur à celui prévu au présent règlement.</p>
-    <h1 class="p-title">ARTICLE 5 – Les dotations</h1>
+    <h2 class="p-title">ARTICLE 5 – Les dotations</h2>
     <p>Il est mis en jeu par une mécanique de « loto » 50 000€ à se partager, selon les paliers suivants :</p>
     <ul>
       <li>2 bons numéros = 10 000€ à se partager parmi tous les participants ayant trouvé 2 bons numéros sur une même grille.</li>
@@ -74,9 +74,9 @@ includeHeader('isNav', 'mentions')
     <p>Dans le cas où un participant remplit plusieurs grilles, s’il obtient plusieurs grilles gagnantes, alors il remporte la somme associée à chaque grille gagnante.</p>
     <p>Les numéros n’ont pas besoin d’être dans l’ordre de tirage au sort.</p>
     <p>La répartition des dotations est déterminée au moment de la rédaction du présent règlement et ne saurait faire l’objet d’une contestation quant à son évaluation.</p>
-    <h1 class="p-title">ARTICLE 6 – Désignation des gagnants</h1>
+    <h2 class="p-title">ARTICLE 6 – Désignation des gagnants</h2>
     <p>Dans les deux semaines suivant la fin du jeu, <b>six (6) numéros gagnants</b> seront tirés au sort sous contrôle d’huissier.</p>
-    <p>Ces 6 (six) numéros seront communiqués sur le site de l’opération <a href="www.loto-paques-hasbro.fr">www.loto-paques-hasbro.fr</a></p>
+    <p>Ces 6 (six) numéros seront communiqués sur le site de l’opération <a href="https://www.loto-paques-hasbro.fr">www.loto-paques-hasbro.fr</a></p>
     <p>Les gagnants du jeu seront ceux qui auront au moins 2 numéros gagnants jusqu’à 6 numéros gagnants. Les gains sont à partager en fonction du nombre de personnes ayant le même nombre de numéros gagnants.</p>
     <p>L’huissier attestera du nombre de gagnants par palier suite au dédoublonnage des gagnants (un même ticket de caisse ne peut pas être utilisé pour ouvrir plusieurs comptes). Si nécessaire, l’huissier procédera également au tirage au sort pour désigner les gagnants des sommes qui n’auraient pas été attribuées après désignation des gagnants par palier.</p>
     <p>Les participants ayant entre 2 et 6 numéros gagnants ou ayant gagné par tirage au sort seront contactés par e-mail dans les 15 jours suivant le tirage au sort pour être informés de leur dotations. Ils disposeront alors d’un délai maximum de 15 jours pour répondre à cet e-mail et confirmer leur gain. Le mail de réponse devra contenir :</p>
@@ -87,28 +87,28 @@ includeHeader('isNav', 'mentions')
     <p>Si un gagnant ne confirme pas son gain en renvoyant son RIB et sa carte d’identité dans les 15 jours suivant la date de l’e-mail d’annonce du gain, il sera relancé à 1 reprise par téléphone au numéro indiqué sur son compte de participation.</p>
     <p>Si le gagnant ne répond toujours pas après l’email et l’appel de relance, la somme sera bloquée et ne pourra être réclamée que jusqu’à la fin de gestion du jeu : le 31/07/2022. Passée cette date, le gain sera définitivement perdu.</p>
     <p>La Société Organisatrice se réserve le droit de vérifier par tous moyens que les gagnants ont participé à l’opération dans des conditions conformes au présent règlement, les participations non-conformes entraînant la disqualification du gagnant et l’annulation de son gain.</p>
-    <h1 class="p-title">ARTICLE 7 – Remise des dotations</h1>
+    <h2 class="p-title">ARTICLE 7 – Remise des dotations</h2>
     <p>Les montants alloués seront transmis par virement bancaire dans un délai de 2 à 4 semaines à compter de la date limite de renvoi du RIB. Toute autre forme de transaction est exclue.</p>
     <p>Les dotations ne pourront en aucun cas être remplacées par un autre lot à la demande du gagnant. Les dotations qui ne pourront être distribuées pour des raisons indépendantes de la volonté de la Société Organisatrice seront perdues pour leurs bénéficiaires et ne seront pas réattribuées.</p>
-    <h1 class="p-title">ARTICLE 8 – Propriété intellectuelle</h1>
+    <h2 class="p-title">ARTICLE 8 – Propriété intellectuelle</h2>
     <p>Toutes les marques, logos et autres signes distinctifs composant l’Offre sont la propriété exclusive de leurs titulaires et sont protégés à ce titre par les dispositions du Code de la propriété intellectuelle. La reproduction et la représentation de tout ou partie de ces éléments sont strictement interdites et constituent une contrefaçon passible de sanctions pénales.</p>
-    <h1 class="p-title">ARTICLE 9 – Règlement</h1>
+    <h2 class="p-title">ARTICLE 9 – Règlement</h2>
     <p>La participation au Jeu implique l’acceptation pleine et entière du présent Règlement (ci-après le « Règlement »).</p>
     <p>Il ne sera répondu à aucune demande (par courrier électronique ou par téléphone) concernant l’interprétation ou l’application du Règlement, concernant les modalités et mécanismes du Jeu.</p>
     <p>Le présent règlement a été déposé auprès de la SCP SYNERGIE HUISSIERS 13 – Huissiers de Justice associés – 21 rue Bonnefoy, 13006 Marseille.</p>
-    <p>Le présent Règlement est également disponible sur le site <a href="www.loto-paques-hasbro.fr">www.loto-paques-hasbro.fr</a> pendant toute la durée du Jeu.</p>
-    <h1 class="p-title">Article 10. – Limite de responsabilité</h1>
+    <p>Le présent Règlement est également disponible sur le site <a href="https://www.loto-paques-hasbro.fr">www.loto-paques-hasbro.fr</a> pendant toute la durée du Jeu.</p>
+    <h2 class="p-title">Article 10. – Limite de responsabilité</h2>
     <p>10.1. – La Société gestionnaire s'engage à mettre tous les moyens en œuvre pour assurer le bon déroulement du Jeu. Néanmoins, si une défaillance technique survenait et affectait le bon déroulement du Jeu dans des conditions indépendantes de la volonté de la Société gestionnaire, cette dernière ne saurait être engagées à l'égard des participants.</p>
     <p>10.2. – Si les coordonnées d’un gagnant sont inexploitables ou si le gagnant ne peut être identifié ni par son nom, ni par son adresse électronique, ni par son numéro de téléphone, il n’appartient pas à la Société gestionnaire de faire des recherches complémentaires afin de retrouver le gagnant indisponible, qui ne pourra pas recevoir sa dotation.</p>
     <p>10.3. – En cas de renonciation expresse du gagnant à bénéficier de sa dotation, celle-ci sera conservée par la Société gestionnaire pourra être utilisée dans le cadre d'une opération ultérieure, si la nature de la dotation le permet, et sans que la responsabilité de la Société gestionnaire et organisatrice ne puisse être engagée de ce fait.</p>
     <p>10.4. - Les messages fournis aux Participants par le Site au moment de leur participation sont automatiques et communiqués sous réserve de vérification par la Société gestionnaire du respect de l’ensemble des dispositions du présent règlement, de l’absence de fraude ou de tout dysfonctionnement informatique.</p>
     <p>10.5. - La Société gestionnaire ne saurait être tenue pour responsable en cas de non-délivrance du courrier électronique annonçant le gain par suite d'une erreur dans l'adresse indiquée par le Participant sur son formulaire de participation au Jeu, en cas de défaillance du fournisseur d'accès, en cas de défaillance du réseau internet ou pour tout autre cas.</p>
     <p>7.6. -Les limites de responsabilité stipulées au présent article au profit de la Société gestionnaire bénéficient également à la Société Organisatrice</p>
-    <h1 class="p-title">ARTICLE 11 – Force majeure et modifications</h1>
+    <h2 class="p-title">ARTICLE 11 – Force majeure et modifications</h2>
     <p>La Société gestionnaire ne saurait encourir une quelconque responsabilité si, en cas de force majeure ou d’évènements indépendants de sa volonté ou si les circonstances l’exigent, elle était amenée à annuler le présent Jeu, à l’écourter, le proroger, le reporter, le suspendre ou à en modifier les conditions, sa responsabilité ne pouvant être engagée de ce fait. Elle se réserve, dans tous les cas, la possibilité de prolonger la période de participation. Les limites de responsabilité stipulées au présent article au profit de la Société gestionnaire bénéficient également à la Société Organisatrice.</p>
-    <h1 class="p-title">ARTICLE 12 – Exonération de responsabilité</h1>
+    <h2 class="p-title">ARTICLE 12 – Exonération de responsabilité</h2>
     <p>La Société gestionnaire ne saurait être tenue responsable de tout défaut ou dysfonctionnement des dotations. La Société gestionnaire décline toute responsabilité pour tous les incidents ou accidents qui pourraient survenir au gagnant pendant l’utilisation et/ou la jouissance des dotations. Les limites de responsabilité stipulées au présent article au profit de la Société gestionnaire bénéficient également à la Société Organisatrice.</p>
-    <h1 class="p-title">ARTICLE  13 – Protection des données à caractère personnel</h1>
+    <h2 class="p-title">ARTICLE  13 – Protection des données à caractère personnel</h2>
     <p>Les coordonnées des participants seront traitées conformément à la loi Informatique et Libertés du 6 janvier 1978 et au RGPD. Chaque participant dispose d'un droit d'accès, de rectification, d’opposition ou de suppression des informations le concernant qui pourra être exercé auprès de la Société de Gestion du jeu à l’adresse email <a href="mailto:dpo@take-off.fr">dpo@take-off.fr</a> et à l’adresse postale :</p>
     <p class="address">
           TAKE OFF – SERVICE DPO <br>
@@ -123,7 +123,7 @@ includeHeader('isNav', 'mentions')
       <li>Enfin la troisième, 1 an après la fin du contrat, consiste à anonymiser l’ensemble des données personnelles restantes pour que le consommateur ne puisse plus être identifié d’une quelconque manière que ce soit. Dans le cadre d’une participation non conforme, les documents seront supprimés, les données bancaires rendues inutilisables et les données anonymisées dans un délai d’un an à compter de la fin du contrat</li>
     </ul>
     <p>En cas de problème lié à l’utilisation de leurs données, les participants peuvent introduire une réclamation auprès de la CNIL.</p>
-    <h1 class="p-title">ARTICLE 14 – Contact</h1>
+    <h2 class="p-title">ARTICLE 14 – Contact</h2>
     <p>Pour toute question concernant le jeu, contactez le service consommateur dédié au jeu en écrivant à service.consommateur@take-off.fr en précisant dans l’objet de votre mail « 2970 - Jeu Hasbro Pâques ». Toute réclamation devra être portée à la Société gestionnaire du jeu avant le <b>31/07/2022</b>. Après cette date, aucune réclamation ne sera traitée.</p>
   </div>
 </main>
